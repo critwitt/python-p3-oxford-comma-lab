3f076bfc055a39c3a0ec368bc913e04614a1b63c
Chris Witte <christopher.t.witte@gmail.com>
2023-03-08 10:29:51 -0500

    solution

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        items[len(items) - 1] = f"and {items[len(items) - 1]}"
+        return ", ".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return items[0]
